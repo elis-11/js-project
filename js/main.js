3fa20f9906319748c938167c4a7b4d113dca9cfb
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-08-01 14:38:08 +0200

    const engWord = document.getElementById("eng"),

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const engWord = document.getElementById("eng"),
+  engWord = document.getElementById("rus"),
+  inputs = document.getElementsByClassName("input"),
+  addWordBtn = document.getElementById("word-btn"),
+  table = document.getElementById("table");
