48b11f0303c028b7ec97ad68ccda0c19e87d9dca
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-08-01 17:47:09 +0200

    line-height: 22px;

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log('tralala');
+console.log("tralala");
 const engWord = document.getElementById("eng"),
   rusWord = document.getElementById("rus"),
   inputs = document.getElementsByClassName("input"),
@@ -7,6 +7,23 @@ const engWord = document.getElementById("eng"),
 
 let words;
 let btnDelete;
+
+addButton.addEventListener("click", () => {
+  if (
+    engWord.value.lenght < 1 ||
+    rusWord.value.lenght < 1 ||
+    !isNaN(engWord.value) ||
+    !isNaN(rusWord.value)
+  ) {
+    for (let key of inputs) {
+      key.classList.add("error");
+    }
+  } else {
+    for (let key of inputs) {
+      key.classList.remove("error");
+    }
+  }
+});
 
 const CreateWord = (english, russian) => {
   this.english = english;
